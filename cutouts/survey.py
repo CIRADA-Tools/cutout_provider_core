3fa24c1c7d09190674e814c7759c0d989870ad01
rubiculite <dr_smike@yahoo.ca>
2019-05-27 14:04:47 -0500

    [axof:44] Cutout work.

---
+++ cutouts/survey.py
@@ -0,0 +1,11 @@
+
+
+# abstract class for a survey
+class Survey:
+
+    # grab a cutout of size <size> centered on <position>
+    @staticmethod
+    def get_cutout(position, size):
+
+        raise NotImplementedError("This is an abstract class, should not be used directly")
+
