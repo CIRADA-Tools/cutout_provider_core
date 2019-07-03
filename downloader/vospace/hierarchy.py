72daeccfaf8c8579367b21d143b792924de3c998
rubiculite <dr_smike@yahoo.ca>
2019-07-03 15:38:11 -0500

    [axof:102] VOSpace work.

--- downloader/vospace/hierarchy.py
+++ downloader/vospace/hierarchy.py
@@ -1,20 +1,33 @@
+import os
 import re
 
 import yaml as yml
 
 
-class Hierarchy():
+from abc import ABC, abstractmethod
+class Hierarchy(ABC):
     def __init__(self):
-        self.config = yml.load(open("hierarchy.yml"))
+        super().__init__()
+
+        # load the config file, indepent of where the source code is run...
+        this_source_file_dir = re.sub(r"(.*/).*$",r"\1",os.path.realpath(__file__))
+        self.config = yml.load(open(this_source_file_dir+"hierarchy.yml"))
+
+        # set local (relative) and remote (absolute) paths
         self.local_root  = self.__sanitize_path(self.config['root']['local'])
         self.remote_root = self.__sanitize_path(self.config['root']['remote'])
+
+        # get the base data directory hierarchy
         self.hierarchy = self.config['hierarchy']
+
+        # compile a list of support surveys
         self.surveys = [s.lower() for k in [list(self.hierarchy[b].keys()) for b in self.hierarchy.keys()] for s in k]
 
     def __sanitize_path(self,path):
+        # clean up repeating '/'s with a trailing '/' convention
         return re.sub(r"(/+|/*$)",r"/",path)
 
-    def __get_survey_path(self, survey):
+    def __get_base_survey_path(self, survey):
         if self.has_survey(survey):
             for band in self.hierarchy.keys():
                 if survey.lower() in self.hierarchy[band]:
@@ -34,13 +47,22 @@ class Hierarchy():
         return survey.lower() in self.surveys
 
 
+    @abstractmethod
+    def get_survey_dir(self, survey):
+        pass
+
+
+    def get_survey_dirs(self):
+        return [self.get_survey_dir(s) for s in self.surveys]
+
+
 class LocalDirs(Hierarchy):
     def __init__(self):
         super().__init__()
 
 
-    def get_survey_path(self, survey):
-        path = self._Hierarchy__get_survey_path(survey)
+    def get_survey_dir(self, survey):
+        path = self._Hierarchy__get_base_survey_path(survey)
         if not path is None:
             return self.get_local_root()+path
         return path
@@ -51,8 +73,8 @@ class RemoteDirs(Hierarchy):
         super().__init__()
 
 
-    def get_survey_path(self, survey):
-        path = self._Hierarchy__get_survey_path(survey)
+    def get_survey_dir(self, survey):
+        path = self._Hierarchy__get_base_survey_path(survey)
         if not path is None:
             return self.get_remote_root()+path
         return path
@@ -64,8 +86,8 @@ class LocalCutouts(LocalDirs):
         self.sub_dir = self._Hierarchy__sanitize_path(self.config['catagories']['cutouts']['sub_dir'])
 
 
-    def get_survey_path(self, survey):
-        path = super().get_survey_path(survey)
+    def get_survey_dir(self, survey):
+        path = super().get_survey_dir(survey)
         if not path is None:
             return path+self.sub_dir
         return None
@@ -77,41 +99,44 @@ class RemoteCutouts(RemoteDirs):
         self.sub_dir = self._Hierarchy__sanitize_path(self.config['catagories']['cutouts']['sub_dir'])
 
 
-    def get_survey_path(self, survey):
-        path = super().get_survey_path(survey)
+    def get_survey_dir(self, survey):
+        path = super().get_survey_dir(survey)
         if not path is None:
             return path+self.sub_dir
         return None
 
 
 if __name__ == '__main__':
-    print()
-    print(" *** Hierarchy() Class Test ***")
-    h = Hierarchy()
-    print(f"DATA_ROOT: {h.get_local_root()}")
-    print(f"VOSPACE_ROOT: {h.get_remote_root()}")
-    print(f"Has FirSt: {h.has_survey('FirSt')}")
-    print(f"Has Bart: {h.has_survey('Bart')}")
+    #print()
+    #print(" *** Hierarchy() Class Test ***")
+    #h = Hierarchy()
+    #print(f"DATA_ROOT: {h.get_local_root()}")
+    #print(f"VOSPACE_ROOT: {h.get_remote_root()}")
+    #print(f"Has FirSt: {h.has_survey('FirSt')}")
+    #print(f"Has Bart: {h.has_survey('Bart')}")
 
     print()
     print(" *** LocalDirs(Hierarchy) Class Test ***")
     local = LocalDirs()
-    print(f"get_survey_path('First'): {local.get_survey_path('First')}")
+    print(f"get_survey_dir('First'): {local.get_survey_dir('First')}")
+    print("get_survey_dirs(): \n> "+"\n> ".join(local.get_survey_dirs()))
 
     print()
     print(" *** RemoteDirs(Hierarchy) Class Test ***")
-    local = RemoteDirs()
-    print(f"get_survey_path('PanSTARRS'): {local.get_survey_path('PanSTARRS')}")
+    remote = RemoteDirs()
+    print(f"get_survey_dir('PanSTARRS'): {remote.get_survey_dir('PanSTARRS')}")
+    print("get_survey_dirs(): \n> "+"\n> ".join(remote.get_survey_dirs()))
 
     print()
     print(" *** LocalCutouts(LocalDirs(Hierarchy)) Class Test ***")
     local_cutouts = LocalCutouts()
-    print(f"get_survey_path('SDss'): {local_cutouts.get_survey_path('SDss')}")
+    print(f"get_survey_dir('SDss'): {local_cutouts.get_survey_dir('SDss')}")
+    print("get_survey_dirs(): \n> "+"\n> ".join(local_cutouts.get_survey_dirs()))
 
     print()
     print(" *** RemoteCutouts(RemoteDirs(Hierarchy)) Class Test ***")
     remote_cutouts = RemoteCutouts()
-    print(f"get_survey_path('SDss'): {remote_cutouts.get_survey_path('SDss')}")
+    print("get_survey_dirs(): \n> "+"\n> ".join(remote_cutouts.get_survey_dirs()))
 
 
 
