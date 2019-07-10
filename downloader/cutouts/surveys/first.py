aefe298a5bcb9bf6233e186ab69f2e38361bf6e2
rubiculite <dr_smike@yahoo.ca>
2019-07-10 12:49:44 -0500

    Attaching the following TODO's to Issue #6. ```     def get_fits_header_updates(self,header,position,size):         header_updates = {             'BAND':     ('1.4 GHz', 'Frequency of observation'),             'DATE-OBS': (header['DATE-OBS'], 'Obs. date (yearmonth)'),             # TODO (Issue #6): same as MJD-OB... depricate.             #'MJD': (Time(header['DATE-OBS']).mjd, 'Median MJD of obs month (00:00:00 on 15th)'),             # TODO (Issue #6): this is probably already in the wcs part of the header             'BUNIT': ('Jy/beam', 'Pixel flux unit'),             'BMAJ':  (header['BMAJ'], 'Beam major axis [deg]'),             'BMIN':  (header['BMIN'], 'Beam minor axis [deg]'),             'BPA':   (header['BPA'],  'Beam position angle'),              # TODO: 'FNAME' inadequate for mosaics...             'IMFILE': (header['FIELDNAM'], 'FIRST coadded image')         }         return header_updates ```

--- downloader/cutouts/surveys/first.py
+++ downloader/cutouts/surveys/first.py
@@ -52,9 +52,9 @@ class FIRST(SurveyABC):
         header_updates = {
             'BAND':     ('1.4 GHz', 'Frequency of observation'),
             'DATE-OBS': (header['DATE-OBS'], 'Obs. date (yearmonth)'),
-            # TODO: same as MJD-OB... depricate.
+            # TODO (Issue #6): same as MJD-OB... depricate.
             #'MJD': (Time(header['DATE-OBS']).mjd, 'Median MJD of obs month (00:00:00 on 15th)'),
-            # TODO: this is probably already in the wcs part of the header
+            # TODO (Issue #6): this is probably already in the wcs part of the header
             'BUNIT': ('Jy/beam', 'Pixel flux unit'),
             'BMAJ':  (header['BMAJ'], 'Beam major axis [deg]'),
             'BMIN':  (header['BMIN'], 'Beam minor axis [deg]'),
