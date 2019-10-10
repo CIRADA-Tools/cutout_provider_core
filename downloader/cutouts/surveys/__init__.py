bc53ed52201043a917e2adeeaec65b37468f7eba
rubiculite <dr_smike@yahoo.ca>
2019-10-09 22:06:18 -0500

    UofA/M Cutout Provider Integration Work...

--- downloader/cutouts/surveys/__init__.py
+++ downloader/cutouts/surveys/__init__.py
@@ -1,8 +1,15 @@
 # abstract base class components
 from .survey_abc import processing_status
 
-from .vlass import VLASS
+# support surveys
+from .first     import FIRST
+from .nvss      import NVSS
+from .panstarrs import PanSTARRS
+from .sdss      import SDSS
+from .vlass     import VLASS
+from .wise      import WISE
 
+# ancillary functions 
 def get_sexadecimal_string(position): 
     from .survey_abc import SurveyABC
     return SurveyABC.get_sexadecimal_string(position)
