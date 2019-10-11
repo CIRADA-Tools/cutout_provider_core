f8b9eebb35b36a4de1751e8172073e9652c90ec8
rubiculite <dr_smike@yahoo.ca>
2019-10-11 15:35:15 -0500

    UofA/M Cutout Provider Integration Work...

--- downloader/cutouts/surveys/__init__.py
+++ downloader/cutouts/surveys/__init__.py
@@ -1,13 +1,15 @@
 # abstract base class components
-from .survey_abc import processing_status
-from .survey_abc import get_sexadecimal_string
-from .survey_config import get_cutout_filename
+from .survey_abc     import processing_status
+from .survey_abc     import get_sexadecimal_string
+from .survey_filters import wise_filters
+from .survey_filters import grizy_filters
+from .survey_config  import get_cutout_filename
 
-# support surveys
+# supported surveys
 from .first     import FIRST
 from .nvss      import NVSS
-from .panstarrs import PanSTARRS
-from .sdss      import SDSS
 from .vlass     import VLASS
 from .wise      import WISE
+from .panstarrs import PanSTARRS
+from .sdss      import SDSS
 
