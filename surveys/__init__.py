986eaa00391430698c068b318eaad48662a697b7
falon3 <scheers@ualberta.ca>
2019-10-31 10:38:15 -0600

    change import

--- surveys/__init__.py
+++ surveys/__init__.py
@@ -4,7 +4,6 @@ from .survey_abc     import get_sexadecimal_string
 from .survey_filters import wise_filters
 from .survey_filters import grizy_filters
 from .survey_config  import get_cutout_filename
-from .FITS2DImageTools import *
 
 # supported surveys
 from .first     import FIRST
